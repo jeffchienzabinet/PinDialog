f0ac32112f7daeb03381271012d71ed0069dc0b5
jeffchienzabinet <jeff@zabinet.com>
2015-06-02 15:29:18 -0700

    Added maxlength to JS frontend

--- www/pin.js
+++ www/pin.js
@@ -3,11 +3,12 @@ var exec = require('cordova/exec');
 
 module.exports = {
 
-	prompt: function(message, resultCallback, title, buttonLabels) {
+	prompt: function(message, resultCallback, title, buttonLabels, maxlength) {
         var _message = (message || "Message");
         var _title = (title || "Title");
         var _buttonLabels = (buttonLabels || ["OK","Cancel"]);
-        cordova.exec(resultCallback, null, "PinDialog", "prompt", [_message, _title, _buttonLabels]);
+        var _maxlength = (maxlength || 0);
+        cordova.exec(resultCallback, null, "PinDialog", "prompt", [_message, _title, _buttonLabels, _maxlength]);
     }
 
 };
